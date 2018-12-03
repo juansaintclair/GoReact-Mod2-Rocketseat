514bb169ffb38d47071b76c3990fc4e58d4411ac
Victor Hermes <me.victorhermes@gmail.com>
2018-12-03 21:58:32 -0200

    ESLint configurado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Olá :)</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Olá amigos!</h1>
+  </div>
+);
 
 export default App;
